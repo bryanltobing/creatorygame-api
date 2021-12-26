a9f9cb3894579466cd4d263b0efe49bb1fe7e02a
Bryan Lumbantobing <bryantobing0@gmail.com>
2021-12-26 15:31:53 +0700

    Add catchAsync util

--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,12 @@ app.use((req, res, next) => {
 })
 
 app.use(((err, req, res, next) => {
-  res.status(err.status || 500)
-  res.send({
+  const status = err.status || 500
+
+  res.status(status).send({
     error: {
       message: err.message,
-      status: err.status,
+      status,
     },
   })
 
--- src/routes/characters.routes.ts
+++ src/routes/characters.routes.ts
@@ -2,49 +2,62 @@ import express from 'express'
 import ApiError from '../errors/ApiError'
 
 import CharacterModel from '../models/character.model'
+import catchAsync from '../utils/catchAsync'
 
 const router = express.Router()
 
 // Get All character
-router.get('/', async (req, res, next) => {
-  const characters = await CharacterModel.find()
-  res.send(characters || [])
-})
+router.get(
+  '/',
+  catchAsync(async (req, res, next) => {
+    const characters = await CharacterModel.find()
+    res.send(characters || [])
+  })
+)
 
 // Get One Character
-router.get('/:slug', async (req, res, next) => {
-  const slug = req.body.slug
+router.get(
+  '/:slug',
+  catchAsync(async (req, res, next) => {
+    const slug = req.body.slug
 
-  if (!slug) {
-    return next(new ApiError(400, 'Slug required'))
-  }
+    if (!slug) {
+      return next(new ApiError(400, 'Slug required'))
+    }
 
-  const character = await CharacterModel.findOne({ slug })
+    const character = await CharacterModel.findOne({ slug })
 
-  if (!character) {
-    return next(new ApiError(404, 'Character not found'))
-  }
-  res.send(character)
-})
+    if (!character) {
+      return next(new ApiError(404, 'Character not found'))
+    }
+    res.send(character)
+  })
+)
 
 // Create Character
-router.post('/', async (req, res) => {
-  const character = new CharacterModel(req.body)
+router.post(
+  '/',
+  catchAsync(async (req, res) => {
+    const character = new CharacterModel(req.body)
 
-  await character.save()
-  res.status(201).send('Character Created')
-})
+    await character.save()
+    res.status(201).send('Character Created')
+  })
+)
 
 // Delete Character
-router.delete('/:slug', async (req, res, next) => {
-  const slug = req.params.slug
+router.delete(
+  '/:slug',
+  catchAsync(async (req, res, next) => {
+    const slug = req.params.slug
 
-  if (!slug) {
-    return next(new ApiError(400, 'Slug required'))
-  }
+    if (!slug) {
+      return next(new ApiError(400, 'Slug required'))
+    }
 
-  await CharacterModel.deleteOne({ slug })
-  res.status(200).send('Character Deleted')
-})
+    await CharacterModel.deleteOne({ slug })
+    res.status(200).send('Character Deleted')
+  })
+)
 
 export default router
