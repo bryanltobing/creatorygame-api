449edee283a49f9abd485b7924458d829cc7030e
Bryan Lumbantobing <bryantobing0@gmail.com>
2021-12-26 16:35:15 +0700

    Add character schema timestamps

--- src/models/character.model.ts
+++ src/models/character.model.ts
@@ -11,33 +11,38 @@ interface Character {
 
 const { Schema } = mongoose
 
-const characterSchema = new Schema<Character>({
-  name: {
-    type: String,
-    required: true,
+const characterSchema = new Schema<Character>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+    },
+    characterCode: {
+      type: Number,
+      enum: [1, 2],
+      default: 1,
+      required: true,
+    },
+    power: {
+      type: Number,
+      min: 0,
+      max: 100,
+      required: true,
+    },
+    wealth: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
   },
-  slug: {
-    type: String,
-    unique: true,
-  },
-  characterCode: {
-    type: Number,
-    enum: [1, 2],
-    default: 1,
-    required: true,
-  },
-  power: {
-    type: Number,
-    min: 0,
-    max: 100,
-    required: true,
-  },
-  wealth: {
-    type: Number,
-    min: 0,
-    required: true,
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 // Hide unecessary mongoose return
 characterSchema.set('toJSON', {
